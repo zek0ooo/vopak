0aeccb83962c2bcc3255af58edc7f663a7135332
Zouher Kabtol <zouher.kabtol@vopak.com>
2020-12-01 08:54:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-// const requestValidator = require('./src/requestValidator');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.post('/device-config', ()=>{});
 
-app.listen(5000, ()=>{
-  console.log('hello'); 
-});
+app.listen(5000, ()=>{});
