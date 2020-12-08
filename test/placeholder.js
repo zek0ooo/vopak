4cb38a04d42cbbd3e9308020e12c1bbc0d536dc7
Zouher Kabtol <zouher.kabtol@vopak.com>
2020-12-08 08:23:14 +0000

    Delete placeholder.js

--- test/placeholder.js
+++
@@ -1,6 +0,0 @@
-const {expect} = require('chai');
-describe('Placeholder test ', function () {
-  it('Ensures there is a test available to be run', async () => {
-    expect(true).to.be.true;
-  });
-});
