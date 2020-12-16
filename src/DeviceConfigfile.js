e32d5fc8e99c604e42f48d949a24a4d7c218a795
Zouher Kabtol <zouher.kabtol@vopak.com>
2020-12-16 23:08:12 +0000

    .

--- src/DeviceConfigfile.js
+++ src/DeviceConfigfile.js
@@ -1,46 +1,32 @@
-// function jsonStructure(inputData){
-//   const obj = {
-//     Devices:[
-//         {
-//             "DeviceName": "EastArea.Tank0",
-//             "DeviceLocation": "nlttr",
-//             "DeviceId": "EastArea.Tank0",
-//             "DeviceType": "Tank",
-//             "EM_FLOC": "TANK0",
-//             "TerminalAssetCode": "XYZ",
-//             "GlobalAssetID": "xyz",
-//             "Tags": [
-//               {
-//                 "TagName": "EastArea.Tank0.level",
-//                 "TagType": "level",
-//                 "MeasurementType": "level",
-//                 "CM_FLOC": "T0_LEVEL",
-//                 "GlobalAssetID": "xyz1",
-//                 "MeasurementMethod": "ullage",
-//                 "MeasurementSpecification": "with high temperature",
-//                 "TagUnitOfMeasure": "CM",
-//                 "MeasurementUnitOfMeasure": "CM"
-//               },
-//               {
-//                 "TagName": "EastArea.Tank0.clock",
-//                 "TagType": "clock",
-//                 "MeasurementType": "clock",
-//                 "CM_FLOC": "T0_CLOCK",
-//                 "GlobalAssetID": "xyz2",
-//                 "MeasurementMethod": "stopwatch",
-//                 "MeasurementSpecification": "UTC",
-//                 "TagUnitOfMeasure": "UTC",
-//                 "MeasurementUnitOfMeasure": "UTC"
-//               }
-//             ]
-//         }
-                
-          
-      
-//     ]
-//   };
-// };
+function jsonStructure(inputData){
+    const device = [];
+    inputData.forEach(element=>{
+        device.push({
+            DeviceName : element.DCS_Controller
+            , DeviceLocation : element.OPC_Tag
+            , DeviceId : element.External_line
+            , DeviceType : element.DeviceType
+            , EM_FLOC : element.EMFLOC
+            , TerminalAssetCode : element.CM_FLOC
+            , GlobalAssetID : ''
+            , Tags : [
+              {
+                TagName : ''
+                , TagType : ''
+                , MeasurementType : element.Type_of_measurement
+                , CM_FLOC : element.CM_FLOC
+                , GlobalAssetID : ''
+                , MeasurementMethod : element.MeasurementMethod
+                , MeasurementSpecification : element.MeasurementSpecification
+                , TagUnitOfMeasure : element.TagUnitOfMeasure
+                , MeasurementUnitOfMeasure : ''
+              }
+            ]
+        });
+    })
+    return device;
+}; 
 
-// module.exports = {
-//   jsonStructure
-// }
+module.exports = {
+  jsonStructure
+}
