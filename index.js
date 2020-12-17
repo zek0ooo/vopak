8360c4e84ba0d255332d9602c7df0fdd26f26a3a
Zouher Kabtol <zouher.kabtol@vopak.com>
2020-12-17 08:35:52 +0000

    .

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const router = require('./src/router/router');
 const fileupload = require('express-fileupload');
 require('./src/mongodb');
 require('dotenv').config();
-const {PORT, HOST} = process.env;
+const {API_PORT, API_HOST} = process.env;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(fileupload());
 app.use(router);
-app.listen(PORT, HOST, ()=>{
+app.listen(API_PORT, API_HOST, ()=>{
   console.log('Server is on');  
 });
