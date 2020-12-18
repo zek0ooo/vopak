591acada50fe690d862e60a38182a8bfc53412a7
Zouher Kabtol <zouher.kabtol@vopak.com>
2020-12-18 00:59:11 +0000

    mongodb

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const {MONGO_USERNAME, MONGO_PASSWORD, DBNAME} = process.env;
-const dbURL = 'mongodb+srv://'+MONGO_USERNAME+':'+MONGO_PASSWORD+'@'+DBNAME+'.ozt4p.azure.mongodb.net/device-config?retryWrites=true&w=majority';
+const {MONGO_USERNAME, MONGO_PASSWORD, MONGO_DB_NAME, MONGO_HOST} = process.env;
+const dbURL = 'mongodb+srv://'+MONGO_USERNAME+':'+MONGO_PASSWORD+'@'+MONGO_HOST+'/'+MONGO_DB_NAME+'?retryWrites=true&w=majority';
 
 mongoose.connect(dbURL, {useNewUrlParser:true, useUnifiedTopology: true})
   .then( () => {
     console.log('Connected to db');
   })
   .catch((err)=>{
-    console.log(err);
+    console.log(err); 
   });
