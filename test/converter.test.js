1f21213b1eb16abf11c7141813fb3fe56e82c160
Richard Davidson <richard@automateers.com>
2020-12-24 10:05:10 +0100

    [tests] fixed tests

--- test/converter.test.js
+++ test/converter.test.js
@@ -1,72 +1,33 @@
 const {expect} = require('chai');
-
 const {convert, validateRow} = require('../src/converter');
 
-
-describe('converter test', ()=>{
-  beforeEach( () => {
-    this.data = {
-      string : 'DCS Controller,OPC Tag,DeviceType,EMFLOC,CM FLOC,Type of measurement,MeasurementMethod,TagUnitOfMeasure,MeasurementSpecification,External line\r\nFCS1121,T_FQR9321860.PV,pipeline,VPL19,FQR-932-18-60,flowrate,sum,MQH,,VPL19\r\nFCS1106,T_PT9321860.PV,pipeline,VPL19,PT-932-18-60,pressure,max,BARG,,VPL19\r\nFCS1121,T_PMP9321901.PV,pump,T_PMP9321901,XI-932-19-01,state,abs,,2 = RUN / 0 = STOP,VPL19',
-      headers : ['DCS Controller', 'OPC Tag', 'DeviceType', 'EMFLOC', 'CM FLOC', 'Type of measurement'],
-      row : ['FCS1121', 'T_PMP9321901.PV', 'pump', 'T_PMP9321901', 'XI-932-19-01', 'state'],
-      errors : ['error on line 2 expected 10 but got 9', 'error on line 3 expected 10 but got 9']
-    };
-    
+describe('converter test', () => {
+  it('should pass with all correct data', () => {
+    const input = [
+      'DCS Controller,OPC Tag,DeviceType,EMFLOC,CM FLOC,Type of measurement,MeasurementMethod,TagUnitOfMeasure,MeasurementSpecification,External line',
+      'FCS1121,T_FQR9321860.PV,pipeline,VPL19,FQR-932-18-60,flowrate,sum,MQH,,VPL19',
+      'FCS1106,T_PT9321860.PV,pipeline,VPL19,PT-932-18-60,pressure,max,BARG,,VPL19',
+      'FCS1121,T_PMP9321901.PV,pump,T_PMP9321901,XI-932-19-01,state,abs,,2 = RUN / 0 = STOP,VPL19'
+    ];
+    expect(convert(input.join("\r\n"))).is.an('array');
   });
-  it('setting headers', () => {
-    expect(convert(this.data.string)).is.an('array');
-  });
-  // it('create error', () => {
-  //   expect(new test(this.data.headers, this.data.row, 1)).to.be.a('string');
-  //
-  // });
 
-  // it('send error', () => {
-  //   expect(new sendError(this.data.errors)).to.throw();
-  //
-  // });it('send error', () => {
-  //   expect(new sendError(this.data.errors)).to.throw();
-  //
-  // });
-  it('send errorrrrrrrrrr', () => {
-    expect(function() {
+  it('should throw an error when data is wrong', () => {
+    expect(function () {
       validateRow([], ['test', 'test2'], 1);
     }).to.throw();
   });
+
+  it('should catch two errors when data is incorrect', () => {
+    const input = [
+      "header1,header2,header3",
+      "column1",
+      "column1,column2",
+      "column1,column2,column3",
+    ];
+
+    expect(function () {
+      convert(input.join("\n"));
+    }).to.throw("Error: error on line 2 expected 3 but got 1, Error: error on line 3 expected 3 but got 2");
+  });
 });
-
-
-
-//   it('line validation returns true when line has same amount of columns as the header', ()=> {
-//    // Arrange
-//     line = "test,test1,test3";
-//     lines = [{line}];
-//     headersLength = 3;    
-   
-//     expect(function() {
-//       // Act
-//       lineIdValid(lines,headersLength);
-//     }).to.equal(true); // Assert
-//   })
-
-//   it('line validation returns false when line does not have the same amount of columns as the header', ()=> {
-//     // Arrange
-//      line = "test,test1,test3";
-//      lines = [{line}];
-//      headersLength = 4;    
-    
-//      expect(function() {
-//        // Act
-//        lineIdValid(lines,headersLength);
-//      }).to.equal(false); // Assert
-//    })
-
-//    it('spaces in the header columns are replced with underscore', ()=> {
-//     // Arrange
-//      firstline = "some header,header2,header3";
-      
-//      expect(function() {
-//        // Act
-//        lineIdValid(firstline);
-//      }).to.equal(false); // Assert
-//    })
