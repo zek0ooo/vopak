e292e89308bf84cad3fa11453b6cfdeabf706e8a
Zouher Kabtol <zouher.kabtol@vopak.com>
2021-01-05 10:14:41 +0000

    .

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {post, get, postUser} = require('../controllers/controller');
+const {post, get} = require('../controllers/controller');
 router.post('/device-config', post);
-router.post('/device-config/login', postUser);
 router.get('/device-config', get);
 module.exports = router; 
  
